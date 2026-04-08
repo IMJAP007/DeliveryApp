093c6d725d4f3ba563efb6407a134f54421d24c8
IMJAP007 <miarpm5@gmail.com>
2026-04-08 21:13:20 +0300

    Добавлен класс Delivery

---
+++ DeliveryApp/Delivery.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryApp
+{
+    public enum DeliveryStatus
+    {
+        Новый,
+        В_пути,
+        Доставлен
+    }
+    public class Delivery
+    {
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public DeliveryStatus Status { get; set; }
+        public Delivery(string customerName, string address, DateTime deliveryDate)
+        {
+            CustomerName = customerName;
+            Address = address;
+            DeliveryDate = deliveryDate;
+            Status = DeliveryStatus.Новый;
+        }
+        public void UpdateStatus(DeliveryStatus newStatus)
+        {
+            Status = newStatus;
+        }
+    }
+}
