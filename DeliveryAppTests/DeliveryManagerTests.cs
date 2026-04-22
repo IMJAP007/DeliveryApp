680e82156c4c0574b082f4c43f2972a1161d7555
Михайлов Аркадий <miarpm5@gmail.com>
2026-04-22 15:24:13 +0300

    Добавлен тест на проверку сохранения достваок в файл при добавлении достваки (DeliveryManagerTests)

--- DeliveryAppTests/DeliveryManagerTests.cs
+++ DeliveryAppTests/DeliveryManagerTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,15 @@ namespace DeliveryAppTests
     [TestClass]
     public class DeliveryManagerTests
     {
+        [TestInitialize]
+        public void SetUp()
+        {
+            if (File.Exists("deliveries.txt"))
+            {
+                File.Delete("deliveries.txt");
+            }
+        }
+
         [TestMethod]
         public void AddDelivery_ValidDelivery_Adds_to_List()
         {
@@ -30,6 +40,28 @@ namespace DeliveryAppTests
             DeliveryManager dm = new DeliveryManager();
 
             dm.AddDelivery(null);
+        }
+
+        [TestMethod]
+        public void AddDelivery_SavesToFile()
+        {
+            DeliveryManager dm = new DeliveryManager();
+
+            Delivery delivery1 = new Delivery("Андрей", "Бармалеева улица", DateTime.Now);
+            Delivery delivery2 = new Delivery("Николай", "улица Рубинштейна", DateTime.Now);
+
+            dm.AddDelivery(delivery1);
+            dm.AddDelivery(delivery2);
+
+            Assert.IsTrue(File.Exists("deliveries.txt"));
+
+            if (File.Exists("deliveries.txt"))
+            {
+                var lines = File.ReadLines("deliveries.txt");
+                
+                Assert.IsTrue(lines.Contains($"Андрей|Бармалеева улица|{DateTime.Now.ToString("yyyy-MM-dd")}|0"));
+                Assert.IsTrue(lines.Contains($"Николай|улица Рубинштейна|{DateTime.Now.ToString("yyyy-MM-dd")}|0"));
+            }
         }
     }
 }
