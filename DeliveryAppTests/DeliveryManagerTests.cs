0b45436feea04a8b7d41e41bd9604146355a3f0b
Михайлов Аркадий <miarpm5@gmail.com>
2026-04-22 14:45:35 +0300

    Добавлен тест на проверку добавления достваки в спикок (DeliveryManager)

--- DeliveryAppTests/DeliveryManagerTests.cs
+++ DeliveryAppTests/DeliveryManagerTests.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DeliveryApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DeliveryAppTests
 {
-    internal class DeliveryManagerTests
+    [TestClass]
+    public class DeliveryManagerTests
     {
+        [TestMethod]
+        public void AddDelivery_ValidDelivery_Adds_to_List()
+        {
+            DeliveryManager dm = new DeliveryManager();
 
+            Delivery delivery = new Delivery("Андрей", "Бармалеева улица", DateTime.Now);
+
+            dm.AddDelivery(delivery);
+
+            Assert.IsTrue(dm.Deliveries.Contains(delivery));
+        }
     }
 }
