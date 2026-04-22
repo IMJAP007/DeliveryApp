06d1601bfe93c108cb0ff4fd5db8e56321aa3ac5
IMJAP007 <miarpm5@gmail.com>
2026-04-22 23:49:42 +0300

    Добавлен проект для тестирования графического интерфейса программы

---
+++ DeliveryAppUITests/DeliveryFormTests.cs
@@ -0,0 +1,46 @@
+﻿using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.UIA3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace DeliveryAppUITests
+{
+    [TestClass]
+    public class DeliveryFormTests
+    {
+        private FlaUI.Core.Application app;
+        private AutomationBase automation;
+        private Window window;
+
+        [TestInitialize]
+        public void Init()
+        {
+            if (File.Exists("deliveries.txt"))
+            {
+                File.Delete("deliveries.txt");
+            }
+            app = FlaUI.Core.Application.Launch(@"C:\Other\Documents\College\Testing\Лабораторные\Лр2\DeliveryApp\DeliveryApp\bin\Debug\DeliveryApp.exe");
+            
+            automation = new UIA3Automation();
+            window = app.GetMainWindow(automation);
+        }
+
+        
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            app?.Close();
+            app?.Dispose();
+            automation?.Dispose();
+
+            if (File.Exists("deliveries.txt"))
+            {
+                File.Delete("deliveries.txt");
+            }
+        }
+    }
+}
