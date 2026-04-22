13a3af325333157a98bb529fa9be3a90dde2fea1
Михайлов Аркадий <miarpm5@gmail.com>
2026-04-22 13:36:34 +0300

    Файл UnitTests1 переименован в DeliveryTests. Добавлена ссылка на DeliveryApp. Написан тест на проверку работы конструктора.

---
+++ DeliveryAppTests/DeliveryTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using DeliveryApp;
+
+namespace DeliveryAppTests
+{
+    [TestClass]
+    public class DeliveryTests
+    {
+        [TestMethod]
+        public void Create_Delivery_Works_Correct_Status_Equals_New()
+        {
+            Delivery delivery = new Delivery("Андрей", "Бармалеева улица", DateTime.Now);
+
+            string expected_name = "Андрей";
+            string actual_name = delivery.CustomerName;
+
+            string expected_address = "Бармалеева улица";
+            string actual_address = delivery.Address;
+
+            DateTime expected_date = DateTime.Now;
+            DateTime actual_date = delivery.DeliveryDate;
+
+            DeliveryStatus expected_status = DeliveryStatus.Новый;
+            DeliveryStatus actual_status = delivery.Status;
+
+            Assert.AreEqual(expected_name, actual_name);
+            Assert.AreEqual (expected_address, actual_address);
+            Assert.AreEqual(expected_date, actual_date);
+            Assert.AreEqual(expected_status, actual_status);
+        }
+    }
+}
